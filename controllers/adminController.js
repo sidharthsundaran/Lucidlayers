afafb0426ab7471116ff79a1eb815d9a97a8d20e
Sidharth Sundaran <post4sidharrth@gmail.com>
2024-12-18 10:42:23 +0530

    produclt list page

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -142,7 +142,7 @@ const renderUsers=async(req,res)=>{
 
 const renderProduct = async (req,res)=>{
     const productData= await Products.find()
-    res.render('productlist',{data:productData})
+    res.render('productList',{data:productData})
 }
 
 
